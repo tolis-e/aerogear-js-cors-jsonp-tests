f175c9b6c36cd974163230d06b3801aab91d336e
tolis-e <tolisemm@yahoo.gr>
2013-06-18 17:24:35 +0200

    Initial commit

---
+++ aerogear-rest-service/src/main/java/org/jboss/aerogear/rest/model/Member.java
@@ -0,0 +1,59 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2012, Red Hat, Inc., and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.jboss.aerogear.rest.model;
+
+public class Member {
+
+    private String id;
+
+    private String description;
+
+    public Member() {
+    }
+
+    public Member(String description) {
+        this.description = description;
+    }
+
+    public Member(String id, String description) {
+        this.id = id;
+        this.description = description;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return "Data{ id=" + id + ", description=" + description + '}';
+    }
+
+}
